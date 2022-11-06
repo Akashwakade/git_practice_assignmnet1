2842f856bb5a25358baf305d2287dc07fc3bd9b5
shailja026 <shailjagupta4466@gmail.com>
2022-11-06 10:20:56 +0530

    code is fixed now

--- prime.js
+++ prime.js
@@ -1,10 +1,16 @@
 //prime
-let n=4;
-let count=0;
-for(i=1;i<=n;i++){
-if(n%i==0){
-count++;}}
-if(count==1){
-console.log("no. is prime")}
-else{
-console.log("no. is not prime")}
+function prime(n){
+ let count=0;
+for(let i=1;i<=n;i++){
+if(n%i===0)
+count++
+}
+return count;
+
+}
+let ans=prime(9);
+if(ans==2){
+console.log("is a prime")
+}else{
+console.log("not a prime")
+}
